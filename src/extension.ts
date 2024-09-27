c69fffba671bd6c0c7fd28c0bb97e71c038f238d
Dan Vanderkam <danvdk@gmail.com>
2024-09-27 13:27:43 -0400

    gpt solution with a type error

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,73 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as ts from 'typescript';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.languages.registerCodeActionsProvider('typescript', {
+    provideCodeActions(document: vscode.TextDocument) {
+      const diagnostics: vscode.Diagnostic[] = [];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "any-xray" is now active!');
+      // Step 1: Create a TypeScript Program
+      const fileName = document.uri.fsPath;
+      const sourceCode = document.getText();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('any-xray.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from any-xray!');
-	});
+      // Create a SourceFile object
+      const sourceFile = ts.createSourceFile(fileName, sourceCode, ts.ScriptTarget.ES2020, true);
 
-	context.subscriptions.push(disposable);
+      // Create a CompilerHost to provide necessary functions to the compiler
+      const compilerHost: ts.CompilerHost = {
+        getSourceFile: (fileName, languageVersion) => {
+          if (fileName === document.uri.fsPath) {
+						return sourceFile;
+					}
+          return undefined;
+        },
+        getDefaultLibFileName: (options) => ts.getDefaultLibFilePath(options),
+        writeFile: (fileName, content) => {},
+        getCurrentDirectory: () => "",
+        getDirectories: (path) => [],
+        getCanonicalFileName: (fileName) => fileName,
+        getNewLine: () => "\n",
+        useCaseSensitiveFileNames: () => true,
+        fileExists: (fileName) => fileName === document.uri.fsPath,
+        readFile: (fileName) => fileName === document.uri.fsPath ? sourceCode : undefined
+      };
+
+      // Create the Program
+      const program = ts.createProgram([fileName], { noEmit: true }, compilerHost);
+
+      // Get the TypeChecker
+      const checker = program.getTypeChecker();
+
+      // Step 2: Traverse the AST and find inferred any types
+      function visit(node: ts.Node) {
+        if (ts.isVariableDeclaration(node) && !node.type) {
+          const symbol = checker.getSymbolAtLocation(node.name);
+          if (symbol) {
+            const type = checker.getTypeOfSymbolAtLocation(symbol, node);
+            const typeString = checker.typeToString(type);
+
+            if (typeString === 'any') {
+              const start = node.getStart();
+              const end = node.getEnd();
+              const range = new vscode.Range(document.positionAt(start), document.positionAt(end));
+
+              const diagnostic = new vscode.Diagnostic(
+                range,
+                `Variable "${node.name.getText()}" is inferred as 'any'`,
+                vscode.DiagnosticSeverity.Warning
+              );
+              diagnostics.push(diagnostic);
+            }
+          }
+        }
+        node.forEachChild(visit);
+      }
+
+      visit(sourceFile);
+
+      return diagnostics;
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
---
+++ src/toy.ts
@@ -0,0 +1,2 @@
+const obj = JSON.parse('{"a": 12}');
+console.log(obj.b);

6d041df48508def23b229bf9c883033318c9f131
Dan Vanderkam <danvdk@gmail.com>
2024-09-28 10:46:23 -0400

    try language services quickInfo; will abandon

--- src/extension.ts
+++ src/extension.ts
@@ -53,7 +53,9 @@ function updateDiagnostics(document: vscode.TextDocument) {
 
   const compilerHost: ts.CompilerHost = {
     getSourceFile: (fileName, languageVersion) => {
-      if (fileName === document.uri.fsPath) return sourceFile;
+      if (fileName === document.uri.fsPath) {
+				return sourceFile;
+			}
       return undefined;
     },
     getDefaultLibFileName: (options) => ts.getDefaultLibFilePath(options),
@@ -69,26 +71,36 @@ function updateDiagnostics(document: vscode.TextDocument) {
 
   const program = ts.createProgram([fileName], { noEmit: true }, compilerHost);
   const checker = program.getTypeChecker();
+	const languageService = ts.createLanguageService(getLanguageServiceHost(program));
 
   // Step 2: Traverse the AST and find inferred any types
   function visit(node: ts.Node) {
-    if (ts.isVariableDeclaration(node) && !node.type) {
+    if ((ts.isVariableDeclaration(node) || ts.isParameter(node)) && !node.type) {
       const symbol = checker.getSymbolAtLocation(node.name);
       if (symbol) {
         const type = checker.getTypeOfSymbolAtLocation(symbol, node);
         const typeString = checker.typeToString(type);
 
         if (typeString === 'any') {
-          const start = node.name.getStart();
-          const end = node.name.getEnd();
-          const range = new vscode.Range(document.positionAt(start), document.positionAt(end));
+					const qi = languageService.getQuickInfoAtPosition(fileName, node.name.getStart());
+					if (!qi?.displayParts) {
+						return;
+					}
+					const flowTypeStr = qi.displayParts.map(dp => dp.text).join('');
+					console.log('any-xray', node.name.getText(), flowTypeStr);
+					const flowType = qi.displayParts.at(-1)?.text;
+					if (flowType === 'any') {
+						const start = node.name.getStart();
+						const end = node.name.getEnd();
+						const range = new vscode.Range(document.positionAt(start), document.positionAt(end));
 
-          const diagnostic = new vscode.Diagnostic(
-            range,
-            `Variable "${node.name.getText()}" is inferred as 'any'`,
-            vscode.DiagnosticSeverity.Warning
-          );
-          diagnostics.push(diagnostic);
+						const diagnostic = new vscode.Diagnostic(
+							range,
+							`Variable "${node.name.getText()}" is inferred as 'any'`,
+							vscode.DiagnosticSeverity.Warning
+						);
+						diagnostics.push(diagnostic);
+					}
         }
       }
     }
@@ -105,4 +117,73 @@ export function deactivate() {
   if (diagnosticCollection) {
     diagnosticCollection.dispose();
   }
+}
+
+// See https://github.com/JoshuaKGoldberg/eslint-plugin-expect-type/blob/a55413/src/rules/expect.ts#L506-L521
+function getLanguageServiceHost(program: ts.Program): ts.LanguageServiceHost {
+  return {
+    getCompilationSettings: () => program.getCompilerOptions(),
+    getCurrentDirectory: () => program.getCurrentDirectory(),
+    getDefaultLibFileName: options => ts.getDefaultLibFilePath(options),
+    getScriptFileNames: () => program.getSourceFiles().map(sourceFile => sourceFile.fileName),
+    getScriptSnapshot: name =>
+      ts.ScriptSnapshot.fromString(program.getSourceFile(name)?.text ?? ''),
+    getScriptVersion: () => '1',
+    // NB: We can't check `program` for files, it won't contain valid files like package.json
+    fileExists: ts.sys.fileExists,
+    readFile: ts.sys.readFile,
+    readDirectory: ts.sys.readDirectory,
+    directoryExists: ts.sys.directoryExists,
+    getDirectories: ts.sys.getDirectories,
+  };
+}
+
+/**
+ * Returns the contextual type of a given node.
+ * Contextual type is the type of the target the node is going into.
+ * i.e. the type of a called function's parameter, or the defined type of a variable declaration
+ */
+export function getContextualType(
+  checker: ts.TypeChecker,
+  node: ts.Expression,
+): ts.Type | undefined {
+  const parent = node.parent;
+
+  if (ts.isCallExpression(parent) || ts.isNewExpression(parent)) {
+    if (node === parent.expression) {
+      // is the callee, so has no contextual type
+      return;
+    }
+  } else if (
+    ts.isVariableDeclaration(parent) ||
+    ts.isPropertyDeclaration(parent) ||
+    ts.isParameter(parent)
+  ) {
+    return parent.type ? checker.getTypeFromTypeNode(parent.type) : undefined;
+  } else if (ts.isJsxExpression(parent)) {
+    return checker.getContextualType(parent);
+  } else if (
+    ts.isIdentifier(node) &&
+    (ts.isPropertyAssignment(parent) ||
+      ts.isShorthandPropertyAssignment(parent))
+  ) {
+    return checker.getContextualType(node);
+  } else if (
+    ts.isBinaryExpression(parent) &&
+    parent.operatorToken.kind === ts.SyntaxKind.EqualsToken &&
+    parent.right === node
+  ) {
+    // is RHS of assignment
+    return checker.getTypeAtLocation(parent.left);
+  } else if (
+    ![ts.SyntaxKind.TemplateSpan, ts.SyntaxKind.JsxExpression].includes(
+      parent.kind,
+    )
+  ) {
+    // parent is not something we know we can get the contextual type of
+    return;
+  }
+  // TODO - support return statement checking
+
+  return checker.getContextualType(node);
 }

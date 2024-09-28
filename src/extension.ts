866c5e0f8eef9b39eab594ce129396331c256535
Dan Vanderkam <danvdk@gmail.com>
2024-09-28 11:43:00 -0400

    playing around with property access

--- src/extension.ts
+++ src/extension.ts
@@ -82,11 +82,18 @@ function updateDiagnostics(document: vscode.TextDocument) {
 
   function visit(node: ts.Node) {
     if (ts.isIdentifier(node)) {
-      const symbol = checker.getSymbolAtLocation(node);
-      if (symbol) {
+			const nodeForSymbol = ts.isPropertyAccessExpression(node.parent) ? node.parent : node;
+      const symbol = checker.getSymbolAtLocation(nodeForSymbol);
+			if (!symbol) {
+				console.log('no symbol found for', node.getText());
+				if (ts.isPropertyAccessExpression(node.parent)) {
+					console.log('parent:', node.parent.getText());
+					console.log('type at location', checker.getTypeAtLocation(node));
+				}
+			} else {
         const type = checker.getTypeOfSymbolAtLocation(symbol, node);
         const typeString = checker.typeToString(type);
-				console.log(node.getText(), typeString);
+				// console.log(node.getText(), typeString);
 
         // Check if the type is inferred as 'any'
         if (typeString === 'any') {

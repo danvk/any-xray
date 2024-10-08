570cdaa1ceffaac64e2f76c725154c11be4473cc
Dan Vanderkam <danvdk@gmail.com>
2024-10-08 09:18:49 -0400

    setting up wrapper class

--- src/interval-set.ts
+++ src/interval-set.ts
@@ -1,33 +1,59 @@
+import { arrayDifference, contains, union } from "interval-operations";
+
 export type Interval = [number, number];
 
-function contains(interval: Interval, point: number) {
-  const [start, end] = interval;
-  return start <= point && point < end;
+/** Closed interval to half-open interval */
+function open([a, b]: Interval): Interval {
+  return [a, b+1];
 }
-
-function overlaps(a: Interval, b: Interval) {
-  const [aStart, aEnd] = a;
-  const [bStart, bEnd] = b;
-  return aStart < bEnd && bStart < aEnd;
+/** Half-open interval to closed interval */
+function close([a, b]: Interval): Interval {
+  return [a, b-1];
 }
 
 export class IntervalSet {
-  // Invariant: intervals are disjoint, sorted.
+  // Invariant: intervals are disjoint, sorted, half-open.
   intervals: Interval[] = [];
 
-  constructor(intervals: Interval[]) {
-    this.intervals = intervals;
+  constructor(intervals?: Interval[]) {
+    for (const interval of intervals ?? []) {
+      this.add(interval);
+    }
+  }
+
+  isEmpty() {
+    return this.intervals.length === 0;
   }
 
   add(interval: Interval) {
-    if (this.intervals.length === 0) {
-      this.intervals.push(interval);
-      return;
+    const openIv = open(interval);
+    this.intervals = union(...this.intervals, openIv);
+  }
+
+  includes(n: number): boolean {
+    for (const [a, b] of this.intervals) {
+      if (n >= a && n < b) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    const [start, end] = interval;
-    // TODO: replace with binary search
-    const i = this.intervals.findIndex((i) => i[0] > start);
+  contains(interval: Interval): boolean {
+    const openIv = open(interval);
+    return this.intervals.some(iv => contains(iv, openIv));
+  }
 
+  /** Returns the parts of interval that are not covered in this set. */
+  uncovered(interval: Interval): IntervalSet {
+    const openIv = open(interval);
+    const uncovered = arrayDifference([openIv], this.intervals);
+    const out = new IntervalSet();
+    out.intervals = uncovered;
+    return out;
+  }
+
+  getIntervals(): Interval[] {
+    return this.intervals.map(close);
   }
 }
--- src/test/interval-set.test.ts
+++ src/test/interval-set.test.ts
@@ -1,12 +1,12 @@
 import { describe, it, expect } from 'vitest';
-import { union, arrayUnion } from 'interval-operations';
 
-type Interval = [number, number];
+import {Interval, IntervalSet} from '../interval-set';
 
 describe('interval-set', () => {
   it('should union intervals', () => {
-    let set: Interval[] = [[1, 3], [5, 7], [9, 11]];
-    set = union(...set, [2, 6]);
-    expect(set).toEqual([[1, 7], [9, 11]]);
+    const set = new IntervalSet([[1, 3], [5, 7]]);
+    expect(set.getIntervals()).toEqual([[1, 3], [5, 7]]);
+    set.add([4, 4]);
+    expect(set.getIntervals()).toEqual([[1, 7]]);
   });
 });
